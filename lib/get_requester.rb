717f1441431a5522084b89006cc66791ce3ccbd8
Max <max.edison@yahoo.com>
2021-12-05 15:32:17 -0500

    commit tree 5be44637547f9f9875cd7d3c9c15287387f94676

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
 # Write your code here
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+class GetRequester
+attr_reader :web_address
+
+def initialize(web_address)
+@web_address = web_address
+end
+
+def get_response_body
+    uri = URI.parse(@web_address)
+    response = Net::HTTP.get_response(uri)
+    response.body
+end
+
+def parse_json
+    JSON.parse(get_response_body)
+end
+
+end
